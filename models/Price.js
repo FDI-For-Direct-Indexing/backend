5b7d5227272dd8d468da088a08bf9acba4be2369
HyeonJu LEE <wkdal3512@dankook.ac.kr>
2024-06-24 19:30:23 +0900

    ✨ feat: Price 스키마에 compare 요소 추가(전일대비 가격)

--- models/Price.js
+++ models/Price.js
@@ -2,13 +2,17 @@ const { mongo, default: mongoose } = require("mongoose");
 const { monkeyRankingDbConnection } = require("./db");
 
 const PriceSchema = new mongoose.Schema(
-  { 
+  {
     corporate_id: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "Corporate",
       required: true,
     },
     price: {
+      type: Number,
+      required: true,
+    },
+    compare: {
       type: Number,
       required: true,
     },
