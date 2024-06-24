3709ee53a80fe0628f6bda2d3dcd68f85fd5d919
HyeonJu LEE <wkdal3512@dankook.ac.kr>
2024-06-24 19:37:15 +0900

    ✨ feat: 현재가 받아오는 RestAPI 구현을 위한 액세스 토큰 발급 로직 구현

--- service/koreainvestmentAPI/accessToken.js
+++ service/koreainvestmentAPI/accessToken.js
@@ -1,37 +1,41 @@
-const axios = require('axios');
+const axios = require("axios");
 
 // 실전 Domain URL
-const apiURL = 'https://openapi.koreainvestment.com:9443/oauth2/tokenP';
+const apiURL = "https://openapi.koreainvestment.com:9443/oauth2/tokenP";
 
 // 요청 본문 데이터
 const requestData = {
-  grant_type: 'client_credentials',
-  appkey: process.env.APP_KEY, 
-  appsecret: process.env.SECRET_KEY 
+  grant_type: "client_credentials",
+  appkey: process.env.APP_KEY,
+  appsecret: process.env.SECRET_KEY,
 };
 
 // 요청 헤더 설정
 const headers = {
-  'Content-Type': 'application/json; charset=UTF-8'
+  "Content-Type": "application/json; charset=UTF-8",
 };
 
-var accessToken = null;
+var accessToken =
+  "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ0b2tlbiIsImF1ZCI6ImYwMGZhYWNiLTVhMzAtNDRmZS1hNjEwLWU2MjFmOGE3ZWUxYiIsInByZHRfY2QiOiIiLCJpc3MiOiJ1bm9ndyIsImV4cCI6MTcxOTI3MTgwMCwiaWF0IjoxNzE5MTg1NDAwLCJqdGkiOiJQU3F2NTJza1VnZjZCcWVTSDVZcUh1VGk3OUVlRHV3bkJXTjEifQ.nClPt01IYMWuEuxKmtKPf06hFjSBCtMTfPYJVrqu7PIYA1MQdotBmxBI4S2XOqM0JoYFvZ_5y_tLWUKozaT6RQ";
 
 // 접근 토큰 발급 함수
 async function getAccessToken() {
   try {
     if (accessToken) {
-      console.log('기존 액세스 토큰 재사용');
-      console.log('Access Token:', accessToken);
+      console.log("기존 액세스 토큰 재사용");
+      console.log("Access Token:", accessToken);
       return accessToken;
     }
-    console.log('새 액세스 토큰 발급');
+    console.log("새 액세스 토큰 발급");
     const response = await axios.post(apiURL, requestData, { headers });
-    console.log('Access Token:', response.data.access_token);
+    console.log("Access Token:", response.data.access_token);
     accessToken = response.data.access_token;
     return accessToken;
   } catch (error) {
-    console.error('Error fetching access token:', error.response ? error.response.data : error.message);
+    console.error(
+      "Error fetching access token:",
+      error.response ? error.response.data : error.message
+    );
     throw error;
   }
 }
