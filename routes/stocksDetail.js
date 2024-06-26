27eb6a355d6fa66cae21501eba254356bee85cbf
kyeongseo90 <rudtj9099@gmail.com>
2024-06-27 08:48:44 +0900

    ✨feat: 지표 이름을 변경하였습니다.

--- routes/stocksDetail.js
+++ routes/stocksDetail.js
@@ -4,13 +4,6 @@ const router = express.Router();
 const { setCurrentStockCode } = require("../service/stocksDetail");
 const { onCodeRetrieved } = require("../service/koreainvestmentAPI/kisSocket");
 
-/**
- * 수익성: proTotalproROE, proTotalproOperatingProfitMargin, prototalproNetProfitMargin
- * 성장성: groTotalInventoryTurnoverPeriod, groTotalisNetIncomeYoY, groTotalisOperatingProfitLossYoY, groTotalisReveneueYoY
- * 안정성: saAverageStaDebtRatio, saAverageStaCurrentRatio
- * 효율성: effInventoryTurnoverPeriod, effPayablesTurnoverPeriod, effReceivablesTurnoverPeriod
- */
-
 /* GET stock */
 router.get("/:code", async function (req, res, next) {
   const stockInfo = await stocks.getStockFundamentals(req.params.code);
--- service/stocksDetail.js
+++ service/stocksDetail.js
@@ -7,31 +7,32 @@ const getStockFundamentals = async (code) => {
     const corporate = await Corporate.findOne({ code: code });
     const stock = await Stock.findOne({ stock: corporate.code });
 
-    const profit_key = ["ROE", "영업이익률", "순이익률"];
-    const growth_key = ["자기자본비율", "영업이익", "영업이익률", "매출액"];
-    const stability_key = ["부채비율", "유동비율"];
+    const profit_key = ["영업이익률", "ROA", "ROE"];
+    const stability_key = ["유동비율", "부채비율"];
+    const growth_key = ["매출액증가율", "영업이익증가율"];
     const efficiency_key = [
-      "재고자산회전일수",
-      "매입채무회전일수",
-      "매출채권회전일수",
+      "총자산회전율",
+      "총부채회전율",
+      "총자본회전율",
     ];
 
     const profit_value = [
-      stock.pro_roe,
-      stock.pro_operating_profit_margin,
-      stock.pro_net_profit_margin,
+      stock.op_profit_margin,
+      stock.roa,
+      stock.roe,
+    ];
+    const stability_value = [
+      stock.cur_ratio,
+      stock.debt_eq_ratio
     ];
     const growth_value = [
-      stock.gro_inventory_turnover_period,
-      stock.gro_is_net_income_yoy,
-      stock.gro_is_operatingprofit_loss_yoy,
-      stock.gro_is_reveneue_yoy,
+      stock.sales_growth_rate,
+      stock.op_income_growth_rate
     ];
-    const stability_value = [stock.sta_debt_ratio, stock.sta_current_ratio];
     const efficiency_value = [
-      stock.eff_inventory_turnover_period,
-      stock.eff_payables_turnover_period,
-      stock.eff_receivables_turnover_period,
+      stock.asset_turnover,
+      stock.debt_turnover,
+      stock.capital_turnover
     ];
 
     const profit = [];
@@ -42,19 +43,19 @@ const getStockFundamentals = async (code) => {
       });
     }
 
-    const growth = [];
-    for (let i = 0; i < growth_key.length; i++) {
-      growth.push({
-        matrix: growth_key[i],
-        rates: growth_value[i],
-      });
-    }
-
     const stability = [];
     for (let i = 0; i < stability_key.length; i++) {
       stability.push({
         matrix: stability_key[i],
         rates: stability_value[i],
+      });
+    }
+
+    const growth = [];
+    for (let i = 0; i < growth_key.length; i++) {
+      growth.push({
+        matrix: growth_key[i],
+        rates: growth_value[i],
       });
     }
 
