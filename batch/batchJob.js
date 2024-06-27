38c8625ebd62a576063d76d8664089d1e2c3e7a5
YaRkyungmin <kkgm94@naver.com>
2024-06-27 10:43:48 +0900

    ✨ feat: batchJob 로직 생성

---
+++ batch/batchJob.js
@@ -0,0 +1,28 @@
+const { downloadCorpCodeXML } = require("./downloadCorpCodeXML");
+const { combineData } = require("./makeOgongCorpListJSON");
+const { handleError } = require("./error/errorHandler");
+const { downloadKopi200ListCSV } = require("./downloadKopi200ListCSV");
+const { convertCsvToJson } = require("./convertKospi200ListCSVIntoJSON");
+const { configureOgongDetailCSV } = require("./configureOgongDetailData");
+const { configureOgongDataCSV } = require("./configureOgongData");
+const { upsertOgongDataToMongo } = require("./upsertOgongDB");
+const { upsertOgongDetailDataToMongo } = require("./upsertOgongDetailDB");
+
+async function runBatchJob() {
+  try {
+    await downloadKopi200ListCSV();
+    await convertCsvToJson();
+    await downloadCorpCodeXML();
+    await combineData();
+    await configureOgongDetailCSV();
+    await configureOgongDataCSV();
+    await upsertOgongDataToMongo();
+    await upsertOgongDetailDataToMongo();
+
+    console.log("끝났나요?");
+  } catch (err) {
+    handleError(err);
+  }
+}
+
+module.exports = { runBatchJob };
