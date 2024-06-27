e6e66f749f63bc5209171bf9ba7195264feff5ae
YaRkyungmin <kkgm94@naver.com>
2024-06-27 10:34:21 +0900

    ✨ feat: ogongService 비즈니스 로직 생성

---
+++ batch/ogongService/Efficiency.js
@@ -0,0 +1,6 @@
+function calculateEfficiency(values) {
+  const sum = values.reduce((acc, val) => acc + parseFloat(val), 0);
+  return (sum / values.length).toFixed(2);
+}
+
+module.exports = calculateEfficiency;
---
+++ batch/ogongService/Profitability.js
@@ -0,0 +1,6 @@
+function calculateProfitability(values) {
+  const sum = values.reduce((acc, val) => acc + parseFloat(val), 0);
+  return (sum / values.length).toFixed(2);
+}
+
+module.exports = calculateProfitability;
