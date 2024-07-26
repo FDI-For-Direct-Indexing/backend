d76a2d8a2305e411331d719dc898f606093529a9
HyeonJu LEE <wkdal3512@dankook.ac.kr>
2024-07-26 18:12:02 +0900

    ✨ feat: 장바구니 라우터 추가

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ const clusterRouter = require("./routes/cluster");
 const stocksDetailRouter = require("./routes/stocksDetail");
 const corporateRouter = require("./routes/corporates");
 const realtimePriceRouter = require("./routes/realtimePrice");
+const cartRouter = require("./routes/cart");
 
 const swaggerUi = require("swagger-ui-express");
 const swaggerJsdoc = require("swagger-jsdoc");
@@ -90,6 +91,8 @@ app.use(errorHandler);
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
 app.use("/api/realtimePrice", realtimePriceRouter);
 app.use("/api/trend", trendRouter);
+app.use("/api/cart", cartRouter);
+
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
---
+++ routes/cart.js
@@ -0,0 +1,5 @@
+const express = require("express");
+const cart = require("../service/cart");
+const router = express.Router();
+
+module.exports = router;
